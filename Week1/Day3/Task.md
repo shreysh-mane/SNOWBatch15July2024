b5c9395177fdd02a7860a32021625f9335b6ef8e
Shreysh Mane <shreysh.mane12@gmail.com>
2024-07-17 09:02:04 +0530

    w1d3 Functions

--- Week1/Day3/Task.md
+++ Week1/Day3/Task.md
@@ -2,7 +2,7 @@
 1.  Calculator programme.
  a. Take input from user which operation he wants to perform. ("sum", "minus", "Mul")
  b. based on his input take the next 2 numbers.
- c. to perform the operation use arrow function. (for sum operation create a sum arrow function and pass 2 values to it form your main funciton and return the sum to your main function. and then console log it.)
+ c. to perform the operation use arrow function. (for sum operation create a sum arrow function and pass 2 values to it form your main funciton and return the sum to your main function. and then console log it. Note: for sum and minus operation use arrowfunction and for mul and div operation use function expression)
 
 
 <!-- Object -->
--- Week1/Day3/index.js
+++ Week1/Day3/index.js
@@ -12,17 +12,17 @@
 // While and Do While
 
 
-var no=10;
+// var no=10;
 
-while(no<10){
-    console.log("While");
-}
+// while(no<10){
+//     console.log("While");
+// }
 
-var no1=10;
-do{
-    console.log("Do While");
-    no1++;
-}while(no1<12);
+// var no1=10;
+// do{
+//     console.log("Do While");
+//     no1++;
+// }while(no1<12);
 
 // 1. While      Gau, Swaroop
 //    Do while
@@ -31,6 +31,56 @@ do{
 // 4. Don't know.
 
 
+// Truthy and falsy values
+// function test(){
+//     if(undefined){
+//         console.log('HI');
+//     }else{
+//         console.log("Bye");
+//     }
+
+// }
+// 1. "HI"        
+// 2. "Bye"      
+// 3. "HI" "Bye" 
+// 4. No idea
+// test();
 
 
+// Functions: 
+
+// 1. Function Declaration:
+
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log(sum(2,2));
+
+// 2. Function expression / anonymous function
+
+var sum= function(a,b){
+    return a+b;
+}
+
+console.log(sum(10,20));
+
+// 3. Arrow function
+
+// var sumArrow=(a,b)=>{
+//     return a+b;
+// }
+
+var sumArrow=(a,b)=>a+b;
+
+console.log(sumArrow(20,30));
+
+
+function cal(){
+    // user Inputs;
+
+    if(userOp=="Sum"){
+        var answer= sumArrow(10,20);
+        console.log(answer);
+    }
+}
 
