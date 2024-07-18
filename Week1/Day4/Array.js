75363125a8ea3454a79a60d5aa43dfe52e8c546f
Shreysh Mane <shreysh.mane12@gmail.com>
2024-07-18 09:36:29 +0530

    Array.js

---
+++ Week1/Day4/Array.js
@@ -0,0 +1,47 @@
+
+// Index starts from 0
+var array1=[1,2,3,{name:"Shreysh",city:"pune"},"HI"];
+
+// console.log(array1[3].name);
+// console.log(array1[0]);
+
+// Modify array elements
+// array1[0]=4;
+// console.log(array1);
+
+// Add an element to an array
+// 1. Push()
+    // array1.push(10);
+    // console.log(array1);
+// 2. unshift()
+    // array1.unshift(40);
+    // console.log(array1);
+
+
+// Remove an element from an array
+// 1. pop()
+    // array1.pop();
+    // array1.pop();
+    // console.log(array1);
+
+// 2. shift()
+// array1.shift()
+// console.log(array1);
+
+// Remove from a particular index
+// splice()
+// array1.splice(2,2);
+// console.log(array1);
+
+// Add at a particular index
+// array1.splice(2,1,1,2);
+// console.log(array1);
+
+// indexof
+
+// var index=array1.indexOf(20);
+// console.log(index);
+
+// for(var i=0;i<array1.length;i++){
+//     console.log(array1[i]);
+// }
