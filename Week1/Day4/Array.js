5eda58c774c958446ea1e2d6b019bcf181c0a801
Shreysh Mane <shreysh.mane12@gmail.com>
2024-07-18 09:41:15 +0530

    ModifyDay4

--- Week1/Day4/Array.js
+++ Week1/Day4/Array.js
@@ -1,47 +1,56 @@
-
 // Index starts from 0
-var array1=[1,2,3,{name:"Shreysh",city:"pune"},"HI"];
+var array1 = [1, 2, 3, {name: "Shreysh", city: "Pune"}, "HI"];
 
-// console.log(array1[3].name);
-// console.log(array1[0]);
+// Accessing elements in the array
+// To access the 'name' property of the object at index 3
+console.log(array1[3].name); // Output: Shreysh
+// To access the first element of the array
+console.log(array1[0]); // Output: 1
 
 // Modify array elements
-// array1[0]=4;
-// console.log(array1);
+// Changing the first element of the array to 4
+array1[0] = 4;
+console.log(array1); // Output: [4, 2, 3, {name: "Shreysh", city: "Pune"}, "HI"]
 
 // Add an element to an array
-// 1. Push()
-    // array1.push(10);
-    // console.log(array1);
-// 2. unshift()
-    // array1.unshift(40);
-    // console.log(array1);
+// 1. Using push() to add an element at the end of the array
+array1.push(10);
+console.log(array1); // Output: [4, 2, 3, {name: "Shreysh", city: "Pune"}, "HI", 10]
 
+// 2. Using unshift() to add an element at the beginning of the array
+array1.unshift(40);
+console.log(array1); // Output: [40, 4, 2, 3, {name: "Shreysh", city: "Pune"}, "HI", 10]
 
 // Remove an element from an array
-// 1. pop()
-    // array1.pop();
-    // array1.pop();
-    // console.log(array1);
+// 1. Using pop() to remove the last element of the array
+array1.pop();
+console.log(array1); // Output: [40, 4, 2, 3, {name: "Shreysh", city: "Pune"}, "HI"]
 
-// 2. shift()
-// array1.shift()
-// console.log(array1);
+// 2. Using shift() to remove the first element of the array
+array1.shift();
+console.log(array1); // Output: [4, 2, 3, {name: "Shreysh", city: "Pune"}, "HI"]
 
-// Remove from a particular index
-// splice()
-// array1.splice(2,2);
-// console.log(array1);
+// Remove elements from a particular index
+// Using splice() to remove 2 elements starting from index 2
+array1.splice(2, 2);
+console.log(array1); // Output: [4, 2, "HI"]
 
-// Add at a particular index
-// array1.splice(2,1,1,2);
-// console.log(array1);
+// Add elements at a particular index
+// Using splice() to add elements at index 2
+array1.splice(2, 1, 1, 2);
+console.log(array1); // Output: [4, 2, 1, 2]
 
-// indexof
+// Finding the index of an element
+// Using indexOf() to find the index of element 20 (which is not in the array)
+var index = array1.indexOf(20);
+console.log(index); // Output: -1
 
-// var index=array1.indexOf(20);
-// console.log(index);
-
-// for(var i=0;i<array1.length;i++){
-//     console.log(array1[i]);
-// }
+// Iterating over the array elements
+// Using a for loop to print each element in the array
+for (var i = 0; i < array1.length; i++) {
+    console.log(array1[i]);
+}
+// Output: 4
+//         2
+//         1
+//         2
--- Week1/Day4/Objects.js
+++ Week1/Day4/Objects.js
@@ -1,46 +1,47 @@
 // Objects
 
-// Creation:
-// Obj literal
-var obj1={
-    name:"Shreysh",
-    city:"Pune",
+// Creation using object literal
+var obj1 = {
+    name: "Shreysh",
+    city: "Pune",
 
-    getInfo: function(){
-     console.log("The name is"+ this.name + "city: "+ this.city);   
+    // Method to get information about the object
+    getInfo: function() {
+        console.log("The name is " + this.name + " and the city is " + this.city);
+    }
+};
+
+// Accessing properties of the object
+console.log(obj1.city); // Output: Pune
+
+// Calling a method of the object
+obj1.getInfo(); // Output: The name is Shreysh and the city is Pune
+
+// Modify the value of properties in the object
+obj1.city = "Karad";
+console.log(obj1.city); // Output: Karad
+
+// Add an additional property to the object
+obj1.company = "ABC";
+console.log(obj1.company); // Output: ABC
+
+// Delete a property from the object
+console.log(obj1); // Output: {name: "Shreysh", city: "Karad", getInfo: ƒ, company: "ABC"}
+delete obj1.company;
+console.log(obj1); // Output: {name: "Shreysh", city: "Karad", getInfo: ƒ}
+
+// Using class to create objects
+class Person {
+
+    // Constructor to initialize properties
+    constructor(personName, personCity) {
+        this.name = personName;
+        this.city = personCity;
     }
 }
 
-console.log(obj1.city);
-obj1.getInfo();
+// Creating a new instance of the Person class
+var obj2 = new Person("Yash", "Mumbai");
 
-// Modify the value of properties in obj
-// obj1.city="Karad";
-// console.log(obj1.city);
-
-// Add additional property to an object
-// obj1.company="ABC";
-// console.log(obj1.company);
-
-// Delete property from object
-// console.log(obj1);
-// delete obj1.company;
-// console.log(obj1);
-
-
-// 2 Class
-// class Person{
-
-//     constructor(personName,personCity){
-//         this.name=personName;
-//         this.city=personCity;
-//     }
-
-// }
-// var obj2= new Person("Yash","Mumbai");
-// console.log(obj2.name);
-
-
-
-
-
+// Accessing properties of the class instance
+console.log(obj2.name); // Output: Yash
