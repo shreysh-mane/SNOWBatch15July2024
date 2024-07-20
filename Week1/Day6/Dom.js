7ae241b66a2c36990accd518a69835695ec23285
Shreysh Mane <shreysh.mane12@gmail.com>
2024-07-20 07:48:55 +0530

    ClosureandHoisting

---
+++ Week1/Day6/Dom.js
@@ -0,0 +1,57 @@
+
+// console.log(document);
+
+// console.log(document.body);
+
+// console.log(document.body.childNodes);
+// console.log(document.body.children[2].children[0]);
+
+
+//__________________
+// Methods for Selecting Elements:
+
+// getElementById: Selects an element by its ID.
+// getElementsByClassName: Selects elements by their class name.
+// getElementsByTagName: Selects elements by their tag name.
+// querySelector: Selects the first element that matches a CSS selector.
+// querySelectorAll: Selects all elements that match a CSS selector.
+
+// Selecting by ID
+// let container = document.getElementById('container');
+
+// Selecting by class name
+// let paragraphs = document.getElementsByClassName('text');
+
+// Selecting by tag name
+// let divs = document.getElementsByTagName('div');
+
+// Selecting with querySelector
+ // querySelector
+ // Id => #<NameOfID>
+ // Class => .<NameOfClass>
+ // Tag => <TagName>
+// let firstParagraph = document.querySelector('.text');
+
+// Selecting with querySelectorAll
+// let allParagraphs = document.querySelectorAll('.text');
+
+
+// __________________
+// var greetEle3= document.getElementById('greetMsg2');
+
+// Access the HTML element content
+// innerHTML , textContent
+// console.log(greetEle.innerHTML);
+// console.log(greetEle3.textContent);
+
+
+// Modify text of an HTML element.
+// greetEle.innerHTML="<ul><li> Bye</li></ul>";
+// greetEle3.textContent="<ul><li> Bye<li></ul>";
+
+
+// Modifying Styles:
+// style property: Allows you to directly set CSS properties.
+// Changing styles
+// firstParagraph.style.color = 'blue';
+// firstParagraph.style.fontSize = '20px';
